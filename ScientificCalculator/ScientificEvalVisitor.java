821bca37dc128a4cf8f4473b1e7e59f45263731d
DavidAvendanoUSA <david.avendano01@usa.edu.co>
2026-08-25 14:59:45 +0000

    11. Parentesis Corregido

--- ScientificCalculator/ScientificEvalVisitor.java
+++ ScientificCalculator/ScientificEvalVisitor.java
@@ -32,7 +32,7 @@ public class ScientificEvalVisitor
 				return left * right;
 			}
 			if (right == 0) {
-				System.out.println("Error: No se puede dividir entre cero.")
+				System.out.println("Error: No se puede dividir entre cero.");
 				return 0.0;
 			} else {
 				return left / right;
@@ -40,7 +40,7 @@ public class ScientificEvalVisitor
 	}
 	@Override
 	public Double visitParens(
-	ScientificClacParser.ParensContexy ctx) {
+	ScientificCalcParser.ParensContext ctx) {
 		return visit (ctx.expr());
 	}
 
